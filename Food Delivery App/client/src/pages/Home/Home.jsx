a9226e1fe55ee968afb812f0e8369b4b2f522cb8
Lokesh-Reddy011 <cs23btech11011@iith.ac.in>
2024-07-09 13:14:47 +0530

    Updated with home page  header

---
+++ Food Delivery App/client/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+  const [category,setcategory]=useState("all");
+  return (
+    <div>
+        <Header />
+        <ExploreMenu category={category} setcategory={setcategory} />
+    </div>
+  )
+}
+
+export default Home
