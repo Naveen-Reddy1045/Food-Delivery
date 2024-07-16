7172abd98272855fc40023e31c9cfb48bc0026b7
Lokesh-Reddy011 <cs23btech11011@iith.ac.in>
2024-07-16 22:19:50 +0530

    Updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,22 +4,18 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Placeholder from './pages/Placeorder/Placeorder'
 import Cart from './pages/Cart/Cart'
-import Help from './pages/Help/Help'
 import Footer from './components/Footer/Footer'
 import Login from './components/Login/Login'
-import Signup from './components/Signup/Signup'
 import Userorder from './pages/Userorder/Userorder.jsx'
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const [login, setlogin] = useState(false);
-  const [signup, setsignup] = useState(false);
   return (
     <>
       <ToastContainer />
       {login ? <Login setlogin={setlogin} /> : <></>}
-      {/* {!signup?<Signup />:<></>} */}
       <div className='app'>
 
         <Navbar setlogin={setlogin} />
@@ -28,7 +24,6 @@ const App = () => {
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<Placeholder />} />
           <Route path='/userorders' element={<Userorder />} />
-          {/* <Route path='/help' element={<Help />} /> */}
         </Routes>
 
 
